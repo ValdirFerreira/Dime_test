82aece50b92b6525c64bad4dd7dde6f63340a246
Valdir Ferreira Gomes da Silva <valdir.silva@fcamara.com.br>
2024-05-13 22:31:40 -0300

    ajusts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IncentivoModel } from 'src/app/models/usuario/IncentivoModel';
 import { LoginService } from 'src/app/services/login.service';
 
 import { Clipboard } from '@angular/cdk/clipboard';
+import { AcoordionModel } from 'src/app/models/AcoordionModel/AcoordionModel';
 
 @Component({
   selector: 'app-home',
@@ -76,41 +77,131 @@ export class HomeComponent implements OnInit {
     this.mapearDivsEAlterarValor0();
     this.mapearDivsEAlterarValor();
 
+    this.showSlides(this.slideIndex);
+
+    this.showSlides2(this.slideIndex2);
+
+    this.carregaListaMock();
+
   }
+
+
+
+
+  slideIndex: number = 1;
+  plusSlides(n: number) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  currentSlide(n: number) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n: number) {
+    let i: number;
+    let slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+    let dots = document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLElement>;
+    if (n > slides.length) { this.slideIndex = 1; }
+    if (n < 1) { this.slideIndex = slides.length; }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[this.slideIndex - 1].style.display = "block";
+    dots[this.slideIndex - 1].className += " active";
+
+
+  }
+
+
+
+  slideIndex2: number = 2;
+  plusSlides2(n: number) {
+    this.showSlides2(this.slideIndex2 += n);
+  }
+
+  currentSlide2(n: number) {
+    this.showSlides2(this.slideIndex2 = n);
+  }
+
+  showSlides2(n: number) {
+    let i: number;
+    let slides = document.getElementsByClassName("mySlides2") as HTMLCollectionOf<HTMLElement>;
+    let dots = document.getElementsByClassName("dot2") as HTMLCollectionOf<HTMLElement>;
+    if (n > slides.length) { this.slideIndex2 = 1; }
+    if (n < 1) { this.slideIndex2 = slides.length; }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[this.slideIndex2 - 1].style.display = "block";
+    dots[this.slideIndex2 - 1].className += " active";
+  }
+
+
+  listAcoordionModel: Array<AcoordionModel>;
+  carregaListaMock() {
+    var listMensagens = [];
+    for (let index = 0; index < 4; index++) {
+
+      var item = new AcoordionModel();
+
+      item.Title = "¿El directorio cuenta con una clasificación por sector económico?";
+      item.Conteudo = "Nam sit amet neque auctor, dignissim augue eu, condimentum justo. Fusce fermentum tempus sapien, a sagittis tellus mattis id. Cras et enim ex.";
+      item.Open = false;
+
+      listMensagens.push(item);
+    }
+
+    this.listAcoordionModel = listMensagens;
+  }
+
+  changerItem(item: AcoordionModel, open: boolean) {
+    item.Open = open;
+  }
+
 
   numero = 1;
   min = 1;
   max = 999999999;
   duracao = 3500; // 4 segundos
 
-   mapearDivsEAlterarValor() {
+  mapearDivsEAlterarValor() {
     const divsValor1 = document.querySelectorAll('.valor1');
-  
+
     divsValor1.forEach((div) => {
       const numeroElement = div.querySelector('span'); // Supondo que o número esteja dentro de uma tag <span> dentro da div
-  
+
       if (numeroElement) {
         const maxValue = parseInt(numeroElement.innerText);
-  
+
         for (let i = this.min; i <= maxValue; i++) {
           setTimeout((nr) => {
             numeroElement.innerHTML = nr.toString() + "K+";
           }, i * this.duracao / maxValue, i);
         }
       }
-    });    
+    });
   }
-  
+
+
+
+
+
 
   mapearDivsEAlterarValor0() {
     const divsValor1 = document.querySelectorAll('.valor0');
-  
+
     divsValor1.forEach((div) => {
       const numeroElement = div.querySelector('span'); // Supondo que o número esteja dentro de uma tag <span> dentro da div
-  
+
       if (numeroElement) {
         const maxValue = parseInt(numeroElement.innerText);
-  
+
         for (let i = this.min; i <= maxValue; i++) {
           setTimeout((nr) => {
             numeroElement.innerHTML = nr.toString() + "+";
@@ -299,7 +390,7 @@ export class HomeComponent implements OnInit {
         break;
 
       case 2:
-         window.open("https://www.facebook.com/sharer/sharer.php?u=" + this.linkGerado + "&quote=" + this.textoEnvio , "_blank");
+        window.open("https://www.facebook.com/sharer/sharer.php?u=" + this.linkGerado + "&quote=" + this.textoEnvio, "_blank");
         // const mensagem = this.textoEnvio;
         // const urlF = this.linkGerado; // URL que você deseja compartilhar
 
@@ -314,13 +405,13 @@ export class HomeComponent implements OnInit {
 
         // window.open(shareUrl, '_blank');
         // const citacao = "Aqui está a minha citação para compartilhar!";
-    
+
         // // Codifique a citação para URL
         // const citaçãoCodificada = encodeURIComponent(citacao);
-        
+
         // // Crie o URL de compartilhamento personalizado com o parâmetro "quote"
         // const urlCompartilhamento = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(this.linkGerado)}&text=${citaçãoCodificada}`;
-        
+
         // // Abra o URL de compartilhamento em uma nova 
         // window.open(urlCompartilhamento, '_blank');
         break;
